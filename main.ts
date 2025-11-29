9cea5531ecba7ea8c7b003e4cb638de97d5ff2e3
Tynasincredible <inesnmmiranda@gmail.com>
2025-11-29 20:13:48 +0000

    Adicionada export function

--- main.ts
+++ main.ts
@@ -4,13 +4,13 @@ namespace MiaTime{
     let _month = 1
     let _year = 2025
 
-    function setDate(day:number, month:number, year:number){
+    export function setDate(day:number, month:number, year:number){
         _day = day;
         _month = month;
         _year = year;
     }
 
-    function getDate(){
+    export function getDate(){
         return _day + '/' + _month + '/' + _year ;
     }
 
